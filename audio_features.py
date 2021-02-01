c961caf0781a2f9f869cb2a7d3e081cedcb860f5
federicoVisi <federico.visi@gmail.com>
2021-02-01 15:39:19 +0100

    Add files via upload

---
+++ audio_features.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A simple module for extracting audio features from an audio file and save them into a pandas dataframe
+
+
+Federico Visi 
+Luleå University of Technology
+
+Created on Wed Jan 27 22:14:48 2021
+
+@author: federicovisi
+"""
+#%%
+import numpy as np
+import pandas as pd
+import librosa
+import sklearn as sk
+
+def normalize(x, axis=0):
+    return sk.preprocessing.minmax_scale(x, axis=axis)
+
+def audio_features(path,hop_length=512):
+    
+    # Load the audio
+    y, sr = librosa.load(path)
+    
+    # Default hop length = 512 samples ~= 23ms at 22050 Hz,
+
+    
+    # Compute spectral centroid
+    spectral_centroid = librosa.feature.spectral_centroid(y=y, 
+                                                          sr=sr)[0]
+    
+    # Compute spectral rollof
+    rolloff = librosa.feature.spectral_rolloff(y=y, 
+                                               sr=sr, 
+                                               roll_percent=0.99)[0]
+    
+    # Compute root-mean-square
+    rms = librosa.feature.rms(y=y)[0]
+    
+    # Compute spectral bandwidth
+    spec_bw = librosa.feature.spectral_bandwidth(y=y, 
+                                                 sr=sr)[0]
+    
+    # Compute spectral flatness
+    flatness = librosa.feature.spectral_flatness(y=y)[0]
+    
+    # Compute zero-crossing rate
+    zcr = librosa.feature.zero_crossing_rate(y)[0]
+    
+    # Compute MFCC (20)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr).T
+    
+    #time vector ToDo
+    #t = np.arange(0 , len(y)/sr , 1/sr) #here the step is 1/sr
+    t2 = np.linspace(0, len(y)/sr, len(spectral_centroid)) #here there are fixed num of steps 
+    # ToDo, maybe steps = len(y)//hop_length more genereal
+    
+    # Collect in pandas dataframe
+    feature_array = [t2, spectral_centroid, rolloff, rms, spec_bw, flatness, zcr, mfcc]
+    
+    feature_index = ['time',
+                     'spectral_centroid',
+                     'rolloff',
+                     'rms',
+                     'spec_bw',
+                     'flatness',
+                     'zcr',
+                     'mfcc']
+    
+    df = pd.DataFrame(data=feature_array,
+                        index=feature_index).T
+    return y, sr, df
+
+
+
+
+
+
+
+
+
---
+++ audio_features_example.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Usage examples of the audio_features module
+
+
+Federico Visi 
+Luleå University of Technology
+
+Created on Mon Feb  1 14:40:38 2021
+
+@author: federicovisi
+"""
+from audio_features import audio_features
+from audio_features import normalize
+
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+
+#%% Function call
+
+# get the path of one of the librosa audio examples
+path = librosa.ex('nutcracker')
+
+# call the audio_features function
+# y = audio; sr = sample rate; df = pandas dataframe containing a time vectore and 26 audio features
+y, sr, df = audio_features(path)
+
+#%% Plot waveform and 3 audio features
+
+librosa.display.waveplot(y, sr=sr, alpha=0.4)
+plt.plot(df['time'].values, normalize(df['spectral_centroid']), color='r')
+plt.plot(df['time'].values, normalize(df['rolloff']), color='g')
+plt.plot(df['time'].values, normalize(df['rms']), color='m')
+
+
